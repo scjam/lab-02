2a9cb2b96da3d8e1fc854548dea20696f385b04c
Sydney James <sydneyjames7@gmail.com>
2020-12-01 15:42:17 -0800

    tests pass, spread used

--- src/copy-and-push/copyAndPush.test.js
+++ src/copy-and-push/copyAndPush.test.js
@@ -1,11 +1,15 @@
 const { copyAndPush } = require('./copyAndPush.js');
 
 describe('copyAndPush', () => {
-    it('returns a new array with all the items in the original array and a new item pushed to the end', () => {
-        const numbers = [1, 2, 3];
+    const numbers = [1, 2, 3];
 
+    it('returns a new array with all the items in the original array and a new item pushed to the end', () => {
         const result = copyAndPush(numbers, 4);
 
         expect(result).toEqual([1, 2, 3, 4])
+    })
+    it('checks that the original array stays the same', () => {
+
+        expect(numbers).toEqual([1, 2, 3])
     })
 })
