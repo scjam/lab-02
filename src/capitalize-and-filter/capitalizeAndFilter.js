8bed87c9a04ca9624d2367447b6d18c302f57406
Sydney James <sydneyjames7@gmail.com>
2020-12-01 16:29:51 -0800

    refactored with array methods

--- src/capitalize-and-filter/capitalizeAndFilter.js
+++ src/capitalize-and-filter/capitalizeAndFilter.js
@@ -1,5 +1,7 @@
-const capitalizeAndFilter = arr => {
-
+const capitalizeAndFilter = (arr) => {
+    return arr
+      .filter((item) => item.charAt(0) !== 'f')
+      .map((item) => item.toUpperCase());
 }
 
 module.exports = {
