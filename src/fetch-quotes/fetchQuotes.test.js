80543b4c418025a97fb8da1f0acf51778b788be3
Sydney James <sydneyjames7@gmail.com>
2020-12-01 17:05:31 -0800

    fetch quotes test passing

--- src/fetch-quotes/fetchQuotes.test.js
+++ src/fetch-quotes/fetchQuotes.test.js
@@ -3,12 +3,7 @@ const request = require('superagent');
 
 describe('fetchQuotes', () => {
     it('returns a single quote as an object with name, text, and image properties', async() => {
-        const testQuote = {
-            name: 'Bender',
-            text: `I'm gonna drink 'til I reboot.`,
-            image: 'https://res.cloudinary.com/dzxqhkyqd/image/fetch/c_scale,w_500/https://res.cloudinary.com/dzxqhkyqd/image/upload/v1552429540/bender.png'
-        };
-
+        
         const result = await fetchQuotes();
 
         expect(result).toEqual({
