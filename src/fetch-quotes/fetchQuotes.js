9bd00dcd3d1693716e1ca0a955e629d859f6d75a
Sydney James <sydneyjames7@gmail.com>
2020-12-03 14:20:13 -0800

    final commit

--- src/fetch-quotes/fetchQuotes.js
+++ src/fetch-quotes/fetchQuotes.js
@@ -2,9 +2,9 @@ const request = require( 'superagent' );
 
 const fetchQuotes = async() => {
    
-    const response = await request.get('http://futuramaapi.herokuapp.com/api/quotes');
+    const response = await request.get('http://futuramaapi.herokuapp.com/api/quotes/1');
 
-    const results = response.body[18]
+    const results = response.body[0]
 
     return quote = {
         name: results.character,
--- src/fetch-quotes/fetchQuotes.test.js
+++ src/fetch-quotes/fetchQuotes.test.js
@@ -1,5 +1,4 @@
 const { fetchQuotes } = require('./fetchQuotes');
-const request = require('superagent');
 
 describe('fetchQuotes', () => {
     it('returns a single quote as an object with name, text, and image properties', async() => {
@@ -7,9 +6,9 @@ describe('fetchQuotes', () => {
         const result = await fetchQuotes();
 
         expect(result).toEqual({
-            name: 'Bender',
-            text: `I'm gonna drink 'til I reboot.`,
-            image: 'https://res.cloudinary.com/dzxqhkyqd/image/fetch/c_scale,w_500/https://res.cloudinary.com/dzxqhkyqd/image/upload/v1552429540/bender.png'
+            name: expect.any(String),
+            text: expect.any(String),
+            image: expect.any(String)
         });
     });
 });
